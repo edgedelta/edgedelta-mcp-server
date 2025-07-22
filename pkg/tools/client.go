df2fa1eeefb0e005f5b05755542e9cd3190e473b
Mustafa Soyvural <soyvural@gmail.com>
2025-07-22 18:05:15 +0100

    fix custom transport

--- pkg/tools/client.go
+++ pkg/tools/client.go
@@ -16,31 +16,51 @@ import (
 )
 
 var (
-	newHTTPClientFunc = func() *http.Client {
-		t := &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			// MaxIdleConnsPerHost does not work as expected
-			// https://github.com/golang/go/issues/13801
-			// https://github.com/OJ/gobuster/issues/127
-			// Improve connection re-use
-			MaxIdleConns: 256,
-			// Observed rare 1 in 100k connection reset by peer error with high number MaxIdleConnsPerHost
-			// Most likely due to concurrent connection limit from server side per host
-			// https://edgedelta.atlassian.net/browse/ED-663
-			MaxIdleConnsPerHost:   128,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-			TLSClientConfig:       &tls.Config{MinVersion: tls.VersionTLS12},
+	newHTTPClientFunc = func(apiTokenHeader string) *http.Client {
+		t := &authedTransport{
+			apiTokenHeader: apiTokenHeader,
+			Transport: http.Transport{
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   30 * time.Second,
+					KeepAlive: 30 * time.Second,
+					DualStack: true,
+				}).DialContext,
+				// MaxIdleConnsPerHost does not work as expected
+				// https://github.com/golang/go/issues/13801
+				// https://github.com/OJ/gobuster/issues/127
+				// Improve connection re-use
+				MaxIdleConns: 256,
+				// Observed rare 1 in 100k connection reset by peer error with high number MaxIdleConnsPerHost
+				// Most likely due to concurrent connection limit from server side per host
+				// https://edgedelta.atlassian.net/browse/ED-663
+				MaxIdleConnsPerHost:   128,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+				TLSClientConfig:       &tls.Config{MinVersion: tls.VersionTLS12},
+			},
 		}
+
 		return &http.Client{Transport: t}
 	}
 )
+
+type authedTransport struct {
+	http.Transport
+	apiTokenHeader string
+}
+
+func (t *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.apiTokenHeader == "" {
+		return t.Transport.RoundTrip(req)
+	}
+
+	if token, ok := tokenKeyFromContext(req.Context()); ok {
+		req.Header.Set(t.apiTokenHeader, token)
+	}
+	return t.Transport.RoundTrip(req)
+}
 
 type HTTPClient struct {
 	cl             *http.Client
@@ -50,7 +70,7 @@ type HTTPClient struct {
 
 func NewHTTPClient(apiURL, apiTokenHeader string) *HTTPClient {
 	return &HTTPClient{
-		cl:             newHTTPClientFunc(),
+		cl:             newHTTPClientFunc(apiTokenHeader),
 		apiURL:         apiURL,
 		apiTokenHeader: apiTokenHeader,
 	}
@@ -66,13 +86,6 @@ func (c *HTTPClient) Get(url string) (*http.Response, error) {
 
 func (c *HTTPClient) APIURL() string {
 	return c.apiURL
-}
-
-func (c *HTTPClient) RoundTrip(req *http.Request) (*http.Response, error) {
-	if token, ok := tokenKeyFromContext(req.Context()); ok {
-		req.Header.Set(c.apiTokenHeader, token)
-	}
-	return c.cl.Transport.RoundTrip(req)
 }
 
 func GetPipelines(ctx context.Context, client Client, opts ...QueryParamOption) ([]PipelineSummary, error) {
